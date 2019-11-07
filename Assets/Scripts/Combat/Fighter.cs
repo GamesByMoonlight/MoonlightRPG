cb7acc1ba7388c5577c5726da7a1e94387f0df5c
Lance <lance.bailey@gmail.com>
2019-11-06 20:18:51 -0600

    30 - Decoupling Dependency Cycles

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -13,9 +13,9 @@ namespace RPG.Combat
 
         private void Update()
         {
-            bool isInRange = Vector3.Distance(target.transform.position, transform.position) < weaponRange;
+            if (target == null) return;
 
-            if (target != null && !isInRange)
+            if (target != null && !GetIsInRange())
             {
                 GetComponent<Mover>().MoveTo(target.position);
             }
@@ -25,9 +25,19 @@ namespace RPG.Combat
             }
         }
 
+        private bool GetIsInRange()
+        {
+            return Vector3.Distance(target.transform.position, transform.position) < weaponRange;
+        }
+
         public void Attack(CombatTarget combatTarget)
         {
             target = combatTarget.transform;
+        }
+
+        public void Cancel()
+        {
+            target = null;
         }
     }
 
