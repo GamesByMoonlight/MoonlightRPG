dd0737088192976152c66911b6178e8d181b48aa
Lockeheede <prescott.lawani@gmail.com>
2019-11-19 20:44:04 -0600

    Revert "27 - Implementing Action Priority"
    This reverts commit 11121deca2c42a4d0fc511b8930edafc626703d8.

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -2,36 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using RPG.Movement;
-using RPG.Combat;
-using System;
 
 namespace RPG.Control
 {
     public class PlayerController : MonoBehaviour
     {
         void Update()
-        {
-            InteractWithCombat();
-            InteractWithMovement();
-        }
-
-        private void InteractWithCombat()
-        {
-            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
-
-            foreach(RaycastHit hit in hits)
-            {
-                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
-                if (target == null) continue;
-
-                if (Input.GetMouseButtonDown(0))
-                {
-                    GetComponent<Fighter>().Attack(target);
-                }
-            }
-        }
-
-        private void InteractWithMovement()
         {
             if (Input.GetMouseButton(0))
             {
@@ -41,17 +17,13 @@ namespace RPG.Control
 
         public void MoveToCursor()
         {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+            bool hasHit = Physics.Raycast(ray, out hit);
             if (hasHit)
             {
                 GetComponent<Mover>().MoveTo(hit.point);
             }
-        }
-
-        private static Ray GetMouseRay()
-        {
-            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 
