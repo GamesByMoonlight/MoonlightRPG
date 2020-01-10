059ba6de5e22da0eec0e46ac20ab63f4eeb92c58
Lance <lance.bailey@gmail.com>
2020-01-09 19:13:00 -0600

    More broken?

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,27 +1,14 @@
-using System;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.AI;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
-
-        enum DestinationIdentifier
-        {
-            A, B, C, D, E
-        }
-
         //In File>Build Settings, make sure to drag the scenes in the proper order
         //To give them the right scene index. That way, you can now load scenes based
         //On their index, incase you change the scene's name
         [SerializeField] int sceneToLoad = -1;
-
-        [SerializeField] Transform spawnPoint;
-
-        [SerializeField] DestinationIdentifier destination;
 
 
 
@@ -34,47 +21,8 @@ namespace RPG.SceneManagement
         {
            if(other.tag == "Player")
            {
-               StartCoroutine(Transition());
+               SceneManager.LoadScene(sceneToLoad);
            }
-        }
-
-        ///Using a Coroutine and IEnummerator to keep the information we had at the start
-        ///Allowing us to keep information for when we start needing the character stats, equip, etc.
-        private IEnumerator Transition()
-        {
-            DontDestroyOnLoad(gameObject);
-            yield return SceneManager.LoadSceneAsync(sceneToLoad);
-
-            Portal otherPortal = GetOtherPortal();
-            UpdatePlayer(otherPortal);
-
-            Destroy(gameObject);
-        }
-
-        ///This piece is for placing the player in the proper position
-        ///Otherwise you would load the scene in whatever position the player
-        ///Is already set in in the scene set up
-        private void UpdatePlayer(Portal otherPortal)
-        {
-            GameObject player = GameObject.FindWithTag("Player");
-            GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
-            //Note the warp was a point they added specifically in section 72
-            //That isn't shown throughout the tutorial. It is for having your spawn point
-            //Acurate without it interfering with the NavMeshAgent, which also Updates the Player position
-            //Rotation is not affected
-            player.transform.rotation = otherPortal.spawnPoint.rotation;
-        }
-
-        private Portal GetOtherPortal()
-        {
-            foreach(Portal portal in FindObjectsOfType<Portal>())
-            {
-                if(portal == this) continue;
-                if(portal.destination != destination) continue;
-
-                return portal;
-            }
-            return null;
         }
     }
 }
